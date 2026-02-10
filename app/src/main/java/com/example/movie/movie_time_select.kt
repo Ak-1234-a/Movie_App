6fe853d94632b1d2866445e97ec98b71152fc996
Arun-Rider <159972471+Arun-Rider@users.noreply.github.com>
2026-02-10 13:43:09 +0530

    added popup menu in timing

--- app/src/main/java/com/example/movie/movie_time_select.kt
+++ app/src/main/java/com/example/movie/movie_time_select.kt
@@ -1,23 +1,47 @@
 package com.example.movie
-import android.content.Intent
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.Gravity
+import android.widget.Button
+import android.widget.PopupMenu
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import android.widget.Button
+import com.example.movie.R
+import com.example.movie.payment
 
 class movie_time_select : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_movie_time_select)
 
         val btn = findViewById<Button>(R.id.button4)
-        btn.setOnClickListener {
-            val intent = Intent(this, payment::class.java)
-            startActivity(intent)
+
+        btn.setOnClickListener { view ->
+
+            val popupMenu = PopupMenu(this, view, Gravity.BOTTOM)
+            popupMenu.menuInflater.inflate(R.menu.popup_menu, popupMenu.menu)
+
+            popupMenu.setOnMenuItemClickListener { item ->
+                when (item.itemId) {
+                    R.id.menu_payment -> {
+                        startActivity(Intent(this, payment::class.java))
+                        true
+                    }
+
+                    R.id.menu_other -> {
+                        true
+                    }
+
+                    else -> false
+                }
+            }
+
+            popupMenu.show()
         }
     }
 }
