d4fc388d74d42b60c1f9bdb399b5acd680a2c932
Arun-Rider <159972471+Arun-Rider@users.noreply.github.com>
2026-02-16 16:50:04 +0530

    Option menu added

--- app/src/main/java/com/example/movie/home.kt
+++ app/src/main/java/com/example/movie/home.kt
@@ -2,20 +2,30 @@ package com.example.movie
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.LinearLayout
 import androidx.activity.OnBackPressedCallback
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 
 class home : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        // Setup Toolbar
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        supportActionBar?.title = "Movie"
 
         findViewById<LinearLayout>(R.id.movie1).setOnClickListener {
             startActivity(Intent(this, movie_time_select::class.java))
         }
 
+        // Back press confirmation
         onBackPressedDispatcher.addCallback(this, object : OnBackPressedCallback(true) {
             override fun handleOnBackPressed() {
                 AlertDialog.Builder(this@home)
@@ -26,5 +36,33 @@ class home : AppCompatActivity() {
                     .show()
             }
         })
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.options_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+
+            R.id.menu_profile -> {
+                return true
+            }
+
+            R.id.menu_settings -> {
+                return true
+            }
+
+            R.id.menu_help -> {
+                return true
+            }
+
+            R.id.menu_logout -> {
+                finishAffinity()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
